a386fa2575eabeffbe53c41723abf1d1e8ac7652
woodwood <543498711@qq.com>
2023-09-28 23:38:35 +0800

    create Stack.h

---
+++ Stack.h
@@ -0,0 +1,21 @@
+//
+// Created by 蒋磊 on 28/9/2023.
+//
+
+#ifndef DATA_STRUCTURE_STACK_H
+#define DATA_STRUCTURE_STACK_H
+
+#endif //DATA_STRUCTURE_STACK_H
+
+#include <stdlib.h>
+
+template<typename T>
+struct Stack
+{
+private:
+    T *data;
+    size_t top; // point to the next spare location
+    size_t capacity;
+
+
+};
